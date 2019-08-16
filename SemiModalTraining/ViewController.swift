a69d1b3a97c306b49bab2fb1f9e095c889ffe130
yochidros <mm9.movement.trb@gmail.com>
2019-08-16 12:11:17 +0900

    create: semi modal training project.

---
+++ SemiModalTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SemiModalTraining
+//
+//  Created by Yoshiki Miyazawa on 2019/08/16.
+//  Copyright © 2019 yochidros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
